2a2b31de102effff36d039b6bed3132ab5be0803
milind19 <38541268+milind19@users.noreply.github.com>
2018-08-24 11:44:13 -0500

    Update: README.md notebooks

--- notebooks/CreateHelloWorld.py
+++ notebooks/CreateHelloWorld.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 DROP TABLE IF EXISTS diamonds;
 
+
 CREATE TABLE diamonds
   USING csv
-  OPTIONS (path "/databricks-datasets/Rdatasets/data-001/csv/ggplot2/diamonds.csv", header "true")
+  OPTIONS (path "/databricks-datasets/Rdatasets/data-001/csv/ggplot2/diamonds01.csv", header "false")
